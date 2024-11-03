107a12c3bc3828a84f4aeb2403a519a7b8b9d3b8
acasmor0802 <cmalfonso@iesrafaelalberti.es>
2024-11-03 18:22:43 +0100

    7

--- src/condicionales/ej21_07.py
+++ src/condicionales/ej21_07.py
@@ -1,16 +1,31 @@
+def calcula(renta):
+    if renta<10000:
+        return "Le corresponde un tipo impositivo del 5%."
+    elif 10000<=renta<20000:
+        return "Le corresponde un tipo impositivo del 15%."
+    elif 20000<=renta<35000:
+        return "Le corresponde un tipo impositivo del 20%."
+    elif 35000<=renta<60000:
+        return "Le corresponde un tipo impositivo del 30%."
+    else:
+        return "Le corresponde un tipo impositivo del 45%."
+
+
 def introduce():
-    sexo= input("Indique su sexo (H/M): ")
-    nombre = input("Escriba su nombre: ")
-    return sexo, nombre
+    renta = None
+    while renta == None:
+        try:
+            renta= float(input("Indique su renta anual: "))
+        except ValueError:
+            renta = None
+            print("**ERROR** No es una renta valida")
+    return renta
+
 
 def main():
-    sexo, nombre = introduce()
-
-    if (sexo=="M" and nombre.lower()<"m") or (sexo=="H" and nombre.lower()>"n"):
-        print("Le corresponde el grupo A.")
-        
-    else:
-        print("Le corresponde el grupo B.")
+    renta = introduce()
+    final = calcula(renta)
+    print(final)
 
 
 if __name__ == "__main__":
