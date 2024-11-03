ed53eee2f5a445d20442d39b6a908b4ab476bcd7
acasmor0802 <cmalfonso@iesrafaelalberti.es>
2024-11-03 18:35:50 +0100

    8

--- src/condicionales/ej21_08.py
+++ src/condicionales/ej21_08.py
@@ -1,48 +1,38 @@
-def mensaje():
-    pass
-
-
-def calc_din(pts: float) -> float:
+def calc_din(pts: float) -> str:
     if pts == 0.0:
         return "Inaceptable, obtienes 0€"
-    if pts == 0.4:
-        return ""
-    if pts == 0.6:
+    elif pts == 0.4:
+        dinero = 2400 * pts
+        return f"Aceptable, obtienes {dinero}€"
+    elif pts >= 0.6:
+        dinero = 2400 * pts
+        return f"Meritorio, obtienes {dinero}€"
+    else:
         raise ValueError("**ERROR** valor desconocido")
+    
 
-
-def comprobar_valor(valor: str):
-    if valor == 0.1:
+def comprobar_valor(valor: float):
+    if valor != 0.0 and valor != 0.4 and valor < 0.6:
         raise ValueError("**ERROR** valor desconocido")
-    if valor == 0.2:
-        raise ValueError("**ERROR** valor desconocido")
-    if valor == 0.3:
-        raise ValueError("**ERROR** valor desconocido")
-    if valor == 0.5:
-        raise ValueError("**ERROR** valor desconocido")
+    
 
+def recib_punt() -> float:
+    valor = None
 
-def recib_punt():
-    valor == None
-
-    while valor == None:
+    while valor is None:
         try:
-            valor = float(input("Introduzca su puntuación: "))
+            valor = float(input("Introduzca su puntuación (0.0, 0.4, 0.6 o más): "))
             comprobar_valor(valor)
         except ValueError as e:
-            if valor is None:
-                print("**ERROR** puntaje no valido, intentalo de nuevo")
-            else:
-                valor = None
-                print(f"**ERROR** {e},intentalo de nuevo")
+            valor = None
+            print(f"**ERROR** {e}, intentalo de nuevo.")
 
+    return valor
 
 def main():
     pts = recib_punt()
     result = calc_din(pts)
-    mensaje(result)
-
-
+    print(result)
 
 if __name__ == "__main__":
     main()
